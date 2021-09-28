32b2c98546fcf77d613bc8fe87c986b7230fcb80
Irving <Irving@PC>
2021-09-27 19:20:47 -0500

    Agregar archivos de proyecto.

---
+++ CalcularAreaWS/Calcular.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalcularAreaWS
+{
+    /// <summary>
+    /// Descripción breve de Calcular
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Calcular : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public decimal areacuadrado(decimal lado)
+        {
+            Calculo c = new Calculo();
+            return c.areacuadrado(lado);
+        }
+
+        [WebMethod]
+        public decimal areatriangulo(decimal basE, decimal heighT)
+        {
+            Calculo c = new Calculo();
+            return c.areatriangulo(basE, heighT);
+        }
+
+        public decimal areacirculo(decimal radiO)
+        {
+            Calculo c = new Calculo();
+            return c.areacirculo(radiO);
+        }
+    }
+}
---
+++ CalcularAreaWS/Calculo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalcularAreaWS
+{
+    public class Calculo
+    {
+        public decimal basE { get; set; }
+        public decimal heighT { get; set; }
+        public decimal radiO { get; set; }
+
+
+
+        public decimal areacuadrado(decimal basE)
+        {
+            return basE * basE;
+        }
+
+        public decimal areatriangulo(decimal basE, decimal heighT)
+        {
+            var resultado = (basE * heighT) / 2;
+            return resultado;
+        }
+
+        public decimal areacirculo(decimal radiO)
+        {
+            var resultado = Math.PI * Math.Pow(Convert.ToDouble(radiO), 2);
+            return Convert.ToDecimal(resultado);
+        }
+    }
+}
